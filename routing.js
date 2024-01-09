bdab80482d8b4685815b31f1a2642b57dd95a061
NABILAFZAL19 <nabilafzal19@gmail.com>
2024-01-10 00:45:38 +0530

    added tenant router

--- routing.js
+++ routing.js
@@ -2,7 +2,8 @@ const express = require("express");
 const routers = express.Router();
 
 const adminRouter = require("./routers/admin");
-
+const tenantRouter = require('./routers/tenants')
 routers.use("/admin", adminRouter);
+routers.use("/tenant", tenantRouter);
 
 module.exports = routers;
